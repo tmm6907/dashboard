1382f085c7b4e88f9345927877e9b70c8cafcc5f
Terrence Moore <tmm6907@gmail.com>
2025-04-01 22:40:26 -0400

    update vite config

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
 		port: 3030,
 		proxy: {
 			'/api': {
-				target: 'http://0.0.0.0:8080',
+				target: 'http://127.0.0.1:8080',
 				changeOrigin: true,
 				secure: false,
 				rewrite: (path) => path.replace(/^\/api/, '')
