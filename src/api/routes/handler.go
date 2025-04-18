081c5ce3c969277727abcb98b6948ac5cb08ae83
Terrence Moore <tmm6907@gmail.com>
2025-04-18 18:17:39 -0400

    modify client token handling

--- src/api/routes/handler.go
+++ src/api/routes/handler.go
@@ -194,6 +194,7 @@ func (h *Handler) GetUserFromToken(tokenString string) (*models.User, error) {
 			db := h.GetDB()
 			var user models.User
 			if err = db.Get(&user, "SELECT * from users WHERE oauth_id = ? LIMIT 1 ;", oauthID); err != nil {
+				log.Info("OAUTH: ", oauthID)
 				return nil, err
 			}
 			return &user, nil
