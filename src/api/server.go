8679c9e366cc97124032e5f82b2d0feca5cdf339
Terrence Moore <tmm6907@gmail.com>
2025-05-24 18:24:57 -0400

    edit compose

--- src/api/server.go
+++ src/api/server.go
@@ -18,7 +18,7 @@ import (
 )
 
 func initDB() (*sqlx.DB, error) {
-	dbName := "mashboard.sqlite"
+	dbName := "db/mashboard.sqlite"
 	buildFile := "build.sql"
 	pragmaCommands := []string{
 		"PRAGMA load_extension = 1;",
