94e5d5fb9424468fe4f748a7bc3513bc72fee981
Terrence Moore <tmm6907@gmail.com>
2025-05-24 16:50:24 -0400

    modify login page 3

--- src/client/src/routes/login/+page.svelte
+++ src/client/src/routes/login/+page.svelte
@@ -1,6 +1,8 @@
 <div class="h-screen grid place-items-center bg-base-300 p-4">
     <div>
-        <h1 class="prose text-2xl">Login</h1>
+        <h1 class="prose text-2xl text-center font-semibold text-primary mb-4">
+            Login
+        </h1>
         <div class="flex items-center">
             <a
                 href="https://api.mashboard.app/auth/login"
