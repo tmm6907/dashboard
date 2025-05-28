da8e570fd867f40238b627c721ec007baf06c914
Terrence Moore <tmm6907@gmail.com>
2025-05-28 11:20:46 -0400

    modify card aspect ratio

--- src/client/src/components/Card.svelte
+++ src/client/src/components/Card.svelte
@@ -60,7 +60,7 @@
                 : "https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"}
             alt={item.alt_text ? item.alt_text : "feed item"}
             loading="lazy"
-            class="h-auto w-full object-cover aspect-video"
+            class="h-auto w-full object-contain aspect-video"
         />
         <span class=" text-wrap line-clamp-2">{item.title}</span>
         <div class="flex justify-between py-2">
