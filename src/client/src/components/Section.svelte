83b0fbcb215ebcc4335c6110dbf2d982d46bd1d6
Terrence Moore <tmm6907@gmail.com>
2025-05-28 13:52:02 -0400

    modify section visuals

--- src/client/src/components/Section.svelte
+++ src/client/src/components/Section.svelte
@@ -13,12 +13,10 @@
         {/if}
     </div>
     <div
-    class="section-container overflow-x-auto pb-8 whitespace-nowrap"
-    class:flex-col={vertical}
-    class:gap-12={vertical}
-    class:flex={!vertical}
-    class:gap-8={!vertical}
-/>
+        class="{vertical ? 'flex-col' : 'flex'} section-container {vertical
+            ? 'gap-12'
+            : 'gap-8'} overflow-x-auto pb-8 whitespace-nowrap"
+    >
         {#if feed}
             {#if items}
                 {#each items.slice(0, vertical ? items.length : max) as item}
